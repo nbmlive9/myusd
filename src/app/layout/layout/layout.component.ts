359767760e150919bedc97f8df48ebffb1a9d89b
nbmlive9 <nbmlive9@gmail.com>
2025-08-07 01:57:10 +0530

    update

--- src/app/layout/layout/layout.component.ts
+++ src/app/layout/layout/layout.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 import { TokenService } from 'src/app/service/token.service';
 import { UserService } from 'src/app/service/user.service';
 
@@ -8,28 +9,59 @@ import { UserService } from 'src/app/service/user.service';
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
   isCollapsed = false;
+  isMobile = false;
   udata: any;
-  constructor(private router:Router,private token:TokenService,private api:UserService){}
-  toggleSidebar() {
-    this.isCollapsed = !this.isCollapsed;
-  }
-  ngOnInit(){
+  activeLink: string = '';
+
+  constructor(private router: Router, private token: TokenService, private api: UserService) {}
+
+  ngOnInit() {
     this.api.home().subscribe((res: any) => {
-      console.log('utree', res);
       this.udata = res.data.profiledata;
-  });
+    });
+
+    this.checkScreenSize();
+      this.handleRouteChanges();
   }
 
-  activeLink: string = ''; 
+  @HostListener('window:resize')
+  onResize() {
+    this.checkScreenSize();
+    
+  }
+
+checkScreenSize() {
+  this.isMobile = window.innerWidth <= 768;
+
+  // ✅ Open sidebar by default in mobile
+  if (this.isMobile) {
+    this.isCollapsed = true; // open
+  } else {
+    this.isCollapsed = true; // also open in desktop
+  }
+}
+
+  handleRouteChanges() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        if (this.isMobile) {
+          this.isCollapsed = false; // Close after navigating
+        }
+      });
+  }
+
+  toggleSidebar() {
+     if (this.isMobile) {
+    this.isCollapsed = !this.isCollapsed;
+    }
+  }
 
   setActive(link: string) {
     this.activeLink = link;
   }
-
- 
-
 
   logout() {
     this.token.signOut();
@@ -37,9 +69,5 @@ export class LayoutComponent {
 
   mytree(regid: string) {
     this.router.navigateByUrl(`/treeview/${regid}`);
-  } 
-
-
-
-
+  }
 }
